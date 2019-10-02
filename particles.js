2a5aa2c2c64d1f19def33d3296c0cbf51585d0af
Evan Shimizu <ebshimizu@gmail.com>
2019-10-02 13:30:39 -0400

    explicit update params function

--- particles.js
+++ particles.js
@@ -158,6 +158,22 @@ var pJS = function(tag_id, params) {
     mode_repulse_distance: pJS.interactivity.modes.repulse.distance
   };
 
+  pJS.fn.updateParams = function(params) {
+    Object.deepExtend(pJS, params);
+    // regen tmp obj?
+    pJS.tmp.obj = {
+      size_value: pJS.particles.size.value,
+      size_anim_speed: pJS.particles.size.anim.speed,
+      move_speed: pJS.particles.move.speed,
+      line_linked_distance: pJS.particles.line_linked.distance,
+      line_linked_width: pJS.particles.line_linked.width,
+      mode_grab_distance: pJS.interactivity.modes.grab.distance,
+      mode_bubble_distance: pJS.interactivity.modes.bubble.distance,
+      mode_bubble_size: pJS.interactivity.modes.bubble.size,
+      mode_repulse_distance: pJS.interactivity.modes.repulse.distance
+    };
+  }
+
   pJS.fn.retinaInit = function() {
     if (pJS.retina_detect && window.devicePixelRatio > 1) {
       pJS.canvas.pxratio = window.devicePixelRatio;
